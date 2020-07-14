4087ebd8c3e3c0e95a77798c15b67b43c9ebe6bc
lovexp1 <44965220+lovexp1@users.noreply.github.com>
2020-07-14 23:49:06 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "@/app.scss";
+import RouterGuard from "@/routers/RouterGuard";
 import { Demo } from "./view/demo/index";
 import { A } from "./view/demo/a";
 import { B } from "./view/demo/b";
@@ -9,8 +10,11 @@ function App() {
     <div>
       <BrowserRouter>
         <Demo></Demo>
-        <Route path="/a" component={A}></Route>
-        <Route path="/b" component={B}></Route>
+        {/* <RouterGuard></RouterGuard> */}
+        <Switch>
+          <Route path="/a" component={A}></Route>
+          <Route path="/b" component={B}></Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
